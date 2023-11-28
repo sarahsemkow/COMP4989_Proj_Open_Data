b35bf359c22664e907a58709b8a7813c24dda530
Sarah Semkow <ssemkow@my.bcit.ca>
2023-11-28 15:14:26 -0800

    Changed this file to only take in one image

--- run_movenet.py
+++ run_movenet.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import pandas as pd
 
 # Dictionary that maps from joint names to keypoint indices.
 KEYPOINT_DICT = {
@@ -50,7 +51,7 @@ KEYPOINT_EDGE_INDS_TO_COLOR = {
 }
 
 
-def movenet(image_path):
+def movenet(image_path, threshold):
     img = Image.open(image_path)
     print(img.mode)
 
@@ -77,66 +78,20 @@ def movenet(image_path):
     # Output is a [1, 1, 17, 3] numpy array.
     # Keypoint coordinates are in the range [0.0, 1.0] normalized by the input image size.
     keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+
+    keypoints_with_scores = keypoints_with_scores.reshape(17, 3)
+    keypoint_coordinates_within_threshold = keypoints_with_scores[keypoints_with_scores[:, 2] > threshold]
+    if keypoint_coordinates_within_threshold.shape[0] < 17:
+        print("Not enough keypoints detected")
+        return
     return keypoints_with_scores
 
 
-def plot_keypoints_and_save(keypoints_with_scores, threshold, path=None):
-    reshaped = keypoints_with_scores.reshape(17, 3)
+filepath = './dataset/downdog/0000_4444.jpg'
 
-    keypoint_coordinates = reshaped[reshaped[:, 2] > threshold]
-    print("original:", reshaped.shape, f"\nreshaped with threshold at {threshold}:", keypoint_coordinates.shape)
-    if reshaped[reshaped[:, 2] > threshold].shape[0] < 17:
-        print("Not enough keypoints detected")
-        return
+keypoints = movenet(filepath, threshold=0.1)
+print(keypoints)
 
-    # keypoint_coordinates = keypoint_coordinates[:, [1, 0]]
-    keypoint_coordinates[:, 0], keypoint_coordinates[:, 1] = keypoint_coordinates[:, 1], -keypoint_coordinates[:, 0]
-
-    plt.figure(figsize=(5, 5))
-
-    # Plot keypoints
-    for keypoint, idx in KEYPOINT_DICT.items():
-        plt.scatter(keypoint_coordinates[idx, 0], keypoint_coordinates[idx, 1], label=keypoint)
-
-    # Connect keypoints with lines based on KEYPOINT_EDGE_INDS_TO_COLOR
-    for edge, color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
-        start_idx, end_idx = edge
-        plt.plot([keypoint_coordinates[start_idx, 0], keypoint_coordinates[end_idx, 0]],
-                 [keypoint_coordinates[start_idx, 1], keypoint_coordinates[end_idx, 1]],
-                 color=color)
-
-    # Set labels and show the plot
-    plt.axis('off')
-    if path:
-        new_root = os.path.dirname(path) + '_stick'
-        os.makedirs(new_root, exist_ok=True)
-        new_file_name = new_root + '/' + os.path.basename(path)
-        plt.savefig(f'{new_file_name}', dpi=300, bbox_inches='tight')
-    # plt.legend()
-    plt.show()
-
-
-def get_keypoints_and_save_image(path, threshold=0.2):
-    # change this path to try out different images
-    keypoints = movenet(path)
-    plot_keypoints_and_save(keypoints, threshold, path)
-    return keypoints
-
-
-directory = './dataset/downdog'
-for index, filename in enumerate(os.listdir(directory)):
-    # if filename.endswith(".jpg"):
-    if filename.endswith(".DS_Store"):
-        continue
-    filepath = os.path.join(directory, filename)
-    print(filepath)
-    print(index)
-    print((index / len(os.listdir(directory))) * 100, '% complete')
-    keypoints = get_keypoints_and_save_image(filepath)
-
-# keypoints = get_keypoints_and_save_image('./dataset/downdog/242424242_440440.jpg')
-# keypoints = get_keypoints_and_save_image('./dataset/downdog/242424242_327327.png')
-
-# file_path_chosen = './dataset/downdog/242424242_315315.jpg'
-# img = Image.open(file_path_chosen)
-# print(img.mode)
+# Write keypoints to csv
+# df = pd.DataFrame(keypoints)
+# df.to_csv('keypoints.csv', index=False)
