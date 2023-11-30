c31b17a9c941d910020dbfce67289388af5163b0
HARO-T14S21CQ\Sharon <harosung@gmail.com>
2023-11-29 23:21:16 -0800

    Refactored keypoint and angle functions

---
+++ keypoint_util.py
@@ -0,0 +1,115 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+from constants import KEYPOINT_DICT
+from run_movenet import get_keypoints_and_save_image
+
+# Maps keypoints for angle calculations
+ANGLE_DICT = {
+    'left_elbow': ['left_shoulder', 'left_elbow', 'left_wrist'],
+    'left_armpit': ['left_elbow', 'left_shoulder', 'left_hip'],
+    'left_waist': ['left_shoulder', 'left_hip', 'left_knee'],
+    'left_knee': ['left_hip', 'left_knee', 'left_ankle'],
+    'right_elbow': ['right_shoulder', 'right_elbow', 'right_wrist'],
+    'right_armpit': ['right_elbow', 'right_shoulder', 'right_hip'],
+    'right_waist': ['right_shoulder', 'right_hip', 'right_knee'],
+    'right_knee': ['right_hip', 'right_knee', 'right_ankle']
+}
+
+OUTPUT_FOLDER = 'csv_outputs/'
+
+
+def keypoints_by_directory(directory):
+    """Used for processing keypoints for images in a directory"""
+    kps = []
+    # Gets keypoints from each image in directory
+    for index, filename in enumerate(os.listdir(directory)):
+        if not filename.endswith(".DS_Store"):
+            filepath = os.path.join(directory, filename)
+            print(filepath)
+            print(index)
+            print((index / len(os.listdir(directory))) * 100, '% complete')
+            kps.append(get_keypoints_and_save_image(filepath)[0][0])
+    return kps
+
+
+def map_coordinates_to_keypoint(keypoints, output_csv=False):
+    # Filter keypoint coordinates(xy) without confidence levels
+    filtered_kps = [[values[:2] for values in kp.tolist()] for kp in keypoints]
+    # Map the x and y values to their keypoints
+    # mapped_kp_coords = map_coordinates_to_keypoint(filtered_kps)
+    mapped_kp_coords = []
+    for xy in filtered_kps:
+        row = {}
+        for i, keypoint in enumerate(KEYPOINT_DICT.keys()):
+            row[f"{keypoint}_x"] = xy[i][0]
+            row[f"{keypoint}_y"] = xy[i][1]
+        mapped_kp_coords.append(row)
+    mapped_kp_coords_df = pd.DataFrame(mapped_kp_coords)
+    if output_csv:
+        coordinate_filename = f"{OUTPUT_FOLDER}keypoint_coordinates.csv"
+        mapped_kp_coords_df.to_csv(coordinate_filename, index=False)
+    return mapped_kp_coords_df
+
+
+def get_angles(mapped_kp_coords, append=False, output_csv=False):
+    """Gets the angles for all rows(snapshots) in a dataframe. Set append to True if original keypoint coordinates
+    should be retained in the resulting DataFrame."""
+    # Apply the calculate_angle function to each row of the DataFrame
+    angle_series = mapped_kp_coords.apply(calculate_angles, axis=1, angle_dict=ANGLE_DICT)
+    angle_df = pd.DataFrame.from_dict(angle_series.to_dict(), orient='index')
+    # Append the angles to the original DataFrame
+    if append:
+        angle_df = pd.concat([mapped_kp_coords, angle_df], axis=1)
+    if output_csv:
+        angle_df.to_csv(f"{OUTPUT_FOLDER}keypoints_with_angles.csv", index=False)
+    return angle_df
+
+
+def tuplize_coordinates(row, keypoint):
+    """Helper function for combining xy coordinates"""
+    x_col = f"{keypoint[0]}_x"
+    y_col = f"{keypoint[0]}_y"
+    return [row[x_col], row[y_col]]
+
+
+def calculate_angles(row, angle_dict):
+    """Helper function to calculate the angles for a row(snapshot)"""
+    angles = {}
+    for angle_key, keypoints in angle_dict.items():
+        keypoint_coords = [tuplize_coordinates(row, [keypoint]) for keypoint in keypoints]
+        angle = calculate_angle_from_coords(*keypoint_coords)
+        angles[angle_key] = angle
+    return angles
+
+
+def calculate_angle_from_coords(coord1, coord2, coord3):
+    """Angle calculation from vectors"""
+    vector1 = np.array(coord1) - np.array(coord2)
+    vector3 = np.array(coord3) - np.array(coord2)
+
+    dot_product = np.dot(vector1, vector3)
+    magnitude1 = np.linalg.norm(vector1)
+    magnitude3 = np.linalg.norm(vector3)
+
+    cos_angle = dot_product / (magnitude1 * magnitude3)
+    # Ensure the cosine value is within the valid range
+    cos_angle = np.clip(cos_angle, -1.0, 1.0)
+    # Calculate the angle in radians
+    angle_rad = np.arccos(cos_angle)
+    # Convert the angle to degrees
+    angle_deg = np.degrees(angle_rad)
+    return angle_deg
+
+
+# Create the output folder if it doesn't exist
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+    print(f"Folder '{OUTPUT_FOLDER}' created.")
+
+kp = keypoints_by_directory('./dataset/subset')
+kp_coordinates = map_coordinates_to_keypoint(kp, output_csv=True)
+angles_df = get_angles(kp_coordinates, append=False, output_csv=True)
+print(angles_df.info())
--- run_movenet.py
+++ run_movenet.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-from constants import KEYPOINT_DICT, KEYPOINT_EDGE_INDS_TO_COLOR, ANGLE_DICT, LIMB_DICT
+from constants import KEYPOINT_DICT, KEYPOINT_EDGE_INDS_TO_COLOR
 
 
 def movenet(image_path):
@@ -66,15 +66,15 @@ def plot_keypoints_and_save(keypoints_with_scores, threshold, path=None):
                  [keypoint_coordinates[start_idx, 1], keypoint_coordinates[end_idx, 1]],
                  color=color)
 
-    # # Set labels and show the plot
-    # plt.axis('off')
-    # if path:
-    #     new_root = os.path.dirname(path) + '_stick'
-    #     os.makedirs(new_root, exist_ok=True)
-    #     new_file_name = new_root + '/' + os.path.basename(path)
-    #     plt.savefig(f'{new_file_name}', dpi=300, bbox_inches='tight')
-    # # plt.legend()
-    # plt.show()
+    # Set labels and show the plot
+    plt.axis('off')
+    if path:
+        new_root = os.path.dirname(path) + '_stick'
+        os.makedirs(new_root, exist_ok=True)
+        new_file_name = new_root + '/' + os.path.basename(path)
+        plt.savefig(f'{new_file_name}', dpi=300, bbox_inches='tight')
+    # plt.legend()
+    plt.show()
 
 
 def get_keypoints_and_save_image(path, threshold=0.2):
@@ -82,121 +82,6 @@ def get_keypoints_and_save_image(path, threshold=0.2):
     keypoints = movenet(path)
     # plot_keypoints_and_save(keypoints, threshold, path)
     return keypoints
-
-def keypoint_coords_to_csv(movenet_keypoints, file_name):
-    # Extract keypoints without confidence levels
-    keypoints_filtered = [[keypoints[:2] for keypoints in sample.tolist()] for sample in movenet_keypoints]
-
-    data = []
-    for keypoints in keypoints_filtered:
-        row = {}
-        for i, keypoint in enumerate(KEYPOINT_DICT.keys()):
-            row[f"{keypoint}_x"] = keypoints[i][0]
-            row[f"{keypoint}_y"] = keypoints[i][1]
-        data.append(row)
-    # Specify the CSV file path
-    csv_file_path = file_name
-
-    # Write data to CSV
-    with open(csv_file_path, mode='w', newline='') as csv_file:
-        fieldnames = [f"{keypoint}_x" for keypoint in KEYPOINT_DICT.keys()] + [f"{keypoint}_y" for keypoint in
-                                                                               KEYPOINT_DICT.keys()]
-        interleaved_fieldnames = [field for pair in
-                                  zip(fieldnames[:len(KEYPOINT_DICT)], fieldnames[len(KEYPOINT_DICT):]) for field in
-                                  pair]
-        writer = csv.DictWriter(csv_file, fieldnames=interleaved_fieldnames)
-        # Write header
-        writer.writeheader()
-        for row in data:
-            # Write keypoints data
-            writer.writerow(row)
-    print(f"Keypoints saved to {csv_file_path}")
-
-def calculate_angle(keypoint1, keypoint2, keypoint3):
-    # Calculate vectors for the two lines
-    vector1 = np.array(keypoint1) - np.array(keypoint2)
-    vector2 = np.array(keypoint3) - np.array(keypoint2)
-
-    # Calculate dot product
-    dot_product = np.dot(vector1, vector2)
-
-    # Calculate magnitudes
-    magnitude1 = np.linalg.norm(vector1)
-    magnitude2 = np.linalg.norm(vector2)
-
-    # Calculate cosine of the angle
-    cos_angle = dot_product / (magnitude1 * magnitude2)
-
-    # Calculate the angle in radians
-    angle_rad = np.arccos(np.clip(cos_angle, -1.0, 1.0))
-
-    # Convert the angle to degrees
-    angle_deg = np.degrees(angle_rad)
-
-    return angle_deg
-
-def extract_keypoint_coords(row, keypoints):
-    coords = []
-    for keypoint in keypoints:
-        x_col = f"{keypoint}_x"
-        y_col = f"{keypoint}_y"
-        coords.append([row[x_col], row[y_col]])
-    return coords
-# Function to calculate angle given keypoint indices
-def calculate_angle(row, angle_dict):
-    angle_results = {}
-    for angle_key, keypoints in angle_dict.items():
-        keypoint_coords = [extract_keypoint_coords(row, [keypoint]) for keypoint in keypoints]
-        keypoint_coords_flattened = [x[0] for x in keypoint_coords]
-        angle = calculate_angle_from_coords(*keypoint_coords_flattened)
-        angle_results[angle_key] = angle
-    return angle_results
-
-# Function to calculate angle from coordinates
-def calculate_angle_from_coords(coord1, coord2, coord3):
-    vector1 = np.array(coord1) - np.array(coord2)
-    vector3 = np.array(coord3) - np.array(coord2)
-
-    dot_product = np.dot(vector1, vector3)
-    magnitude1 = np.linalg.norm(vector1)
-    magnitude3 = np.linalg.norm(vector3)
-
-    cos_angle = dot_product / (magnitude1 * magnitude3)
-    # Ensure the cosine value is within the valid range
-    cos_angle = np.clip(cos_angle, -1.0, 1.0)
-    # Calculate the angle in radians
-    angle_rad = np.arccos(cos_angle)
-    # Convert the angle to degrees
-    angle_deg = np.degrees(angle_rad)
-    return angle_deg
-
-# Step 1, obtain keypoint coordinates from images
-keypoints = []
-directory = './dataset/tree'
-for index, filename in enumerate(os.listdir(directory)):
-    # if filename.endswith(".jpg"):
-    if filename.endswith(".DS_Store"):
-        continue
-    filepath = os.path.join(directory, filename)
-    print(filepath)
-    print(index)
-    print((index / len(os.listdir(directory))) * 100, '% complete')
-    keypoints.append(get_keypoints_and_save_image(filepath)[0][0])
-
-coord_file_name = "keypoint_coordinates.csv"
-keypoint_coords_to_csv(keypoints, coord_file_name)
-
-# Step 2, calculate angles from keypoints
-df = pd.read_csv(coord_file_name)
-# Apply the calculate_angle function to each row of the DataFrame
-angle_series = df.apply(calculate_angle, axis=1, angle_dict=ANGLE_DICT)
-angle_df = pd.DataFrame.from_dict(angle_series.to_dict(), orient='index')
-# Concatenate the angle results to the original DataFrame
-df_with_angles = pd.concat([df, angle_df], axis=1)
-df_with_angles.to_csv("keypoints_with_angles.csv", index=False)
-
-
-
 
 
 # keypoints_original = get_keypoints_and_save_image('./model/sample.jpg')
