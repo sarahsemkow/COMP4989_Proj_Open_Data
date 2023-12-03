900fa21f4254a34f21b5edcc6c8649ac06ed15c4
Gary Au <tcgaryau@gmail.com>
2023-12-03 14:00:57 -0800

    edited the main.py to work on the nano

--- keypoint_util.py
+++ keypoint_util.py
@@ -41,7 +41,8 @@ def map_coordinates_to_keypoint(keypoints, output_csv=False):
     """X and y coordinates are mapped to their respective keypoint"""
     # Filter keypoint coordinates(xy) without confidence levels
     if isinstance(keypoints, list):
-        filtered_kps = [[values[:2] for values in kp.tolist()] for kp in keypoints]
+        filtered_kps = [[values[:2] for values in kp.tolist()]
+                        for kp in keypoints]
     else:
         filtered_kps = [[values[:2] for values in keypoints.tolist()]]
     # Map the x and y values to their keypoints
@@ -63,13 +64,15 @@ def get_angles(mapped_kp_coords, append=False, output_csv=False):
     """Gets the angles for all rows(snapshots) in a dataframe. Set append to True if original keypoint coordinates
     should be retained in the resulting DataFrame."""
     # Apply the calculate_angle function to each row of the DataFrame
-    angle_series = mapped_kp_coords.apply(calculate_angles, axis=1, angle_dict=ANGLE_DICT)
+    angle_series = mapped_kp_coords.apply(
+        calculate_angles, axis=1, angle_dict=ANGLE_DICT)
     angle_df = pd.DataFrame.from_dict(angle_series.to_dict(), orient='index')
     # Append the angles to the original DataFrame
     if append:
         angle_df = pd.concat([mapped_kp_coords, angle_df], axis=1)
     if output_csv:
-        angle_df.to_csv(f"{OUTPUT_FOLDER}keypoints_with_angles.csv", index=False)
+        angle_df.to_csv(
+            f"{OUTPUT_FOLDER}keypoints_with_angles.csv", index=False)
     return angle_df
 
 
@@ -84,7 +87,8 @@ def calculate_angles(row, angle_dict):
     """Helper function to calculate the angles for a row(snapshot)"""
     angles = {}
     for angle_key, keypoints in angle_dict.items():
-        keypoint_coords = [tuplize_coordinates(row, [keypoint]) for keypoint in keypoints]
+        keypoint_coords = [tuplize_coordinates(
+            row, [keypoint]) for keypoint in keypoints]
         angle = calculate_angle_from_coords(*keypoint_coords)
         angles[angle_key] = angle
     return angles
@@ -124,11 +128,14 @@ def predict_class(angles, print_result=True):
     index_of_max = np.argmax(pred_probs, axis=1)[0]
     # Get the class labels
     class_labels = model.classes_
-    pred_label = np.array([class_labels[index_of_max]])  # Replacing model.predict()
+    pred_label = np.array([class_labels[index_of_max]]
+                          )  # Replacing model.predict()
     # Create a DataFrame for better visualization
-    prob_df = pd.DataFrame(pred_probs, columns=[f'{label}(%)' for label in class_labels])
+    prob_df = pd.DataFrame(pred_probs, columns=[
+                           f'{label}(%)' for label in class_labels])
     # Print the probabilities with class labels
-    result_df = pd.concat([pd.DataFrame({'True Label': pred_label}), prob_df], axis=1)
+    result_df = pd.concat(
+        [pd.DataFrame({'True Label': pred_label}), prob_df], axis=1)
     if print_result:
         print(f"Class probabilities:\n{result_df.head()}")
     return result_df
@@ -138,6 +145,3 @@ def predict_class(angles, print_result=True):
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
     print(f"Folder '{OUTPUT_FOLDER}' created.")
-
-
-
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from keypoint_util import process_keypoints_to_angles, predict_class
 from movenet import Movenet
+import os
+from watchdog import get_new_image_name, delete_image
+import joblib
 
 
 def main():
+    image_directory = "captured_image"
+    base_directory = os.getcwd()
+    model = joblib.load('models/svc_model.sav')
+    directory = os.path.join(base_directory, image_directory)
     movenet = Movenet()
-    kp = movenet.get_keypoints_with_scores("dataset/tree/00000003_32.jpg")  # Example with single image
-    # kp = keypoints_by_directory(movenet, 'dataset/subset')  # Example with directory
-    angles = process_keypoints_to_angles(kp, print_result=True)
-    predict_class(angles)
+
+    while True:
+        image_captured = get_new_image_name(directory, image_directory)
+        full_image_dir = os.path.join(directory, image_captured)
+        kp = movenet.get_keypoints_with_scores(
+            full_image_dir)  # Example with single image
+        # kp = keypoints_by_directory(movenet, 'dataset/subset')  # Example with directory
+        angles = process_keypoints_to_angles(kp, print_result=True)
+        predict_class(angles)
+#        if image_captured:
+#            delete_image(full_image_dir)
 
 
 if __name__ == "__main__":
