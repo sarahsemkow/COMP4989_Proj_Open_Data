7a414f5b7cdcdac9174f07e7f1969e504177e659
Elaine Lai <elaineelaii@hotmail.com>
2023-12-06 17:45:39 -0800

    opencv and tensorflow on video feed

--- test_nano/webcam_movenet.py
+++ test_nano/webcam_movenet.py
@@ -71,8 +71,11 @@ interpreter.allocate_tensors()
 camera_pipeline = "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=640, height=480, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, width=(int)640, height=(int)480, format=(string)BGRx ! videoconvert ! appsink"
 
 # Open the camera
-# cap = cv2.VideoCapture(camera_pipeline, cv2.CAP_GSTREAMER)
-cap = cv2.VideoCapture(0)
+# JETSON NANO
+cap = cv2.VideoCapture(camera_pipeline, cv2.CAP_GSTREAMER)
+
+# COMPUTER
+# cap = cv2.VideoCapture(0)
 
 # loop through every single frame in webcam
 while cap.isOpened():
